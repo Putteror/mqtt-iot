32bec55c9d21a690f5157b61409dd522dcc24dc7
naruputter <84891037+naruputter@users.noreply.github.com>
2025-04-16 22:13:03 +0700

    first commit

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 var BROKER_ADDRESS = "tcp://139.59.231.19:1883"
-var COMPUTER_MAC_ADDRESS = "04:7C:16:B6:07:55"
+var COMPUTER_MAC_ADDRESS = "04:7c:16:b6:07:55"
 
 func main() {
 	subscribeData()
